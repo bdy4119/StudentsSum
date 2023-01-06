ba66560fe2e97053d25f12524b317d2f162bb97c
백다연 <112463603+bdy4119@users.noreply.github.com>
2023-01-06 15:46:14 +0900

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -6,16 +6,16 @@ public class MainClass {
 		
 		Student s = new Student();
 		
-		s.name = "È«±æµ¿";
+		s.name = "ÃˆÂ«Â±Ã¦ÂµÂ¿";
 		s.ban = 1;
 		s.num = 1;
 		s.kor = 100;
 		s.eng = 60;
 		s.math = 76;
 		
-		System.out.println("ÀÌ¸§:" + s.name);
-		System.out.println("ÃÑÁ¡:" + s.getTotal(s.kor, s.eng, s.math));
-		System.out.println("Æò±Õ:" + s.getAverage());
+		System.out.println("ì´ë¦„" + s.name);
+		System.out.println("ì´ì :" + s.getTotal(s.kor, s.eng, s.math));
+		System.out.println("í‰ê· :" + s.getAverage());
 		
 	}
 
